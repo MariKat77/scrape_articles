9e1747781b7741b4a0c8b9c93341dc945147d5d5
Mario <mario.1999.mk@gmail.com>
2025-10-29 20:56:09 +0100

    fix: database user name change

--- scrape_articles/settings.py
+++ scrape_articles/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'articlesDB',
-        'USER': 'user',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
